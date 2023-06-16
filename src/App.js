1f4c5c1c57bb68ca6a6b479ecf069016609b3579
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-06-16 12:11:22 +0300

    First Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import cx from 'classnames';
+import { Component,} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class TodoList extends Component {
+    constructor(props) {
+      super(props);
+        this.state = {
+          list: [],
+          inputVal: '',
+          selectedItem: false,
+        };
+      }
+
+      handleChange = (event) =>{
+        this.setState({ inputVal: event.target.value});
+        
+      }
+      handleDone = () => {
+        this.setState({ selectedItem: !this.state.selectedItem })
+        
+        // this.setState{}
+      }
+      handleAdd = () =>{
+        const newTodo = this.state.inputVal
+        this.setState(prevState => ({
+          list: [...prevState.list, newTodo]
+        }));
+      }
+    render() {
+      const { list } = this.state;
+        return (
+            <>
+                <div>
+                    <h2>
+                        Todo List
+                    </h2>
+                </div>
+                <div>
+                    <input
+                      type='text' 
+                      value={this.state.inputVal} 
+                      onChange={this.handleChange} /> 
+                    <button onClick={this.handleAdd}>Add</button>
+                    <p> {} remaining out of {list.length} tasks</p>
+                    <div className='todo-list'>
+                        <ul>
+                         {list.map((item, index) => (
+                          <li key={index} className={this.state.selectedItem ? 'is-done': ''} onClick={this.handleDone(item)}>{item}</li>
+                         ))}
+                        </ul>
+                    </div>
+                </div>
+                <style>{`
+                    .is-done {
+                        text-decoration: line-through;
+                    }
+                `}</style>
+            </>
+        );
+    }
 }
-
-export default App;
